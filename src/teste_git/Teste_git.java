2feda932d539b693fb308fb02c09770d8abbbc03
nmadeirajr <newton.francisco27@gmail.com>
2021-03-25 11:02:06 -0300

    acrescentada mensagem de boa tarde

--- src/teste_git/Teste_git.java
+++ src/teste_git/Teste_git.java
@@ -5,6 +5,7 @@ public class Teste_git {
     public static void main(String[] args) {
         
         System.out.println("Bom dia!");
+        System.out.println("Boa tarde!");
         
     }
     
